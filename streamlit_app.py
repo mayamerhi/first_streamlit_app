19c1cff5eabb792598c967131abb95dc126a9782
Maya Merhi <mayamerhi217@gmail.com>
2022-12-16 09:53:19 -0500

    added stopper to troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # displays normalized json
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
