4c057bf0aa5670ec8a11e237cb41dba7930e9822
Maya Merhi <mayamerhi217@gmail.com>
2022-12-16 10:06:52 -0500

    try and except logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,16 +23,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
-
-
-# normalizes json
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# displays normalized json
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error ()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
